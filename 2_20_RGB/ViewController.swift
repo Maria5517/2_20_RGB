a18f23b692276b8a71ddf775eef2e55c812cf5a7
Maria Masljonok <maria@onoffapp.com>
2021-05-23 10:27:22 +0300

    Initial Commit

---
+++ 2_20_RGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2_20_RGB
+//
+//  Created by Maria Masljonok on 23.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
