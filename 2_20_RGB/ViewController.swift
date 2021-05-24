25f4618ae53be798656c9bccdb5d8521191a2f26
Maria Masljonok <maria@onoffapp.com>
2021-05-24 09:47:01 +0300

    added color changing in colorView

--- 2_20_RGB/ViewController.swift
+++ 2_20_RGB/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueLabel: UILabel!
     
-    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
@@ -26,20 +25,32 @@ class ViewController: UIViewController {
         
         colorView.layer.cornerRadius = 20
         
-        redSlider.thumbTintColor = .systemGreen
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 1
-        
-        
-        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        //
+        setColor()
+        //        setValue(for: redLabel, greenLabel, blueLabel)
     }
-
+    
+    // Изменение цветов слайдерами
+    func setColor() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
     @IBAction func rgbSlider(_ sender: UISlider) {
-        redLabel.text = String(redSlider.value)
-        greenLabel.text = String(greenSlider.value)
-        blueLabel.text = String(blueSlider.value)
-        
+        setColor()
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
     }
     
 }
+    
+
+  
+
 
